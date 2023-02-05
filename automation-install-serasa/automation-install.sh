4788b21ef85b641d3b23a4d33da718e415c8e63c
Jeferson W. Candido <39382578+JefersonWC@users.noreply.github.com>
2023-02-04 23:48:19 -0300

    Update automation-install.sh

--- automation-install-serasa/automation-install.sh
+++ automation-install-serasa/automation-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo amazon-linux-extras install ansible2 -y
+
 sudo mv -f hosts /etc/ansible/hosts
 sudo ansible-playbook install-ansible.yml -vvvv
 sudo ansible-playbook install-docker.yml -vvvv
